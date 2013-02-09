707eb7be02b86e88c10f01dd1db027e9a1410a4f
Adam Sutton <dev@adamsutton.me.uk>
2013-02-09 00:24:36 +0000

    buildroot: initial stab at buildroot config for TBS MOI testing

---
+++ buildroot/README.md
@@ -0,0 +1,37 @@
+Tvheadend buildroot config
+==========================
+
+1. Copy package/tvheadend/ to your buildroot
+2. Ensure that the following options are enabled in buildroot config:
+  - Toolchain > Enable large file
+  - Toolchain > Enable IPV6 support
+3. Optionally you can enable:
+  - imagecache : Package > Libraries > Networking > libcurl
+  - avahi : Package > Networking applications > avahi
+  - zlib: Package > Libraries > Compression > zlib
+
+TBS MOI
+-------
+
+For compiling for the TBS MOI you need to configure buildroot as follows:
+
+- Target Architecture > ARM (little endian)
+- Target Architecture Variant > cortex-A8
+- Target ABI > EABI
+
+Once compiled you can simply copy the binary and init scripts to the box
+
+- build/target/usr/bin/tvheadend -> MOI:/usr/bin/tvheadend
+- build/target/etc/default/tvheadend -> MOI:/etc/default/tvheadend
+- build/target/etc/init.d/S99tvheadend -> MOI:/etc/init.d/S99tvheadend
+- build/target/root/.hts/tvheadend/accesscontrol/1 MOI:/root/.hts/tvheadend/accesscontrol/1
+- build/target/usr/lib/ MOI:/usr/lib
+
+Note: if installing onto a pre-configured MOI image you probably have to
+remove /etc/init.d/S99tv
+
+Note: The last step (accesscontrol) only needs to be done once
+
+Note: timeshift is currently disabled
+
+Note: dvbcsa is still not linked, so descrambling might be an issue
---
+++ buildroot/package/tvheadend/tvheadend.mk
@@ -0,0 +1,59 @@
+#############################################################
+#
+# tvheadend
+#
+##############################################################
+
+TVHEADEND_VERSION           = master
+TVHEADEND_SITE              = https://github.com/tvheadend/tvheadend/archive/$(TVHEADEND_VERSION).tar.gz?
+TVHEADEND_LICENSE           = GPLv3+
+TVHEADEND_LICENSE_FILES     = LICENSE
+TVHEADEND_DEPENDENCIES      = host-pkgconf host-python openssl
+
+# Configure options and dependencies
+TVHEADEND_CONFIGURE_OPTS    = --enable-bundle --disable-libav --disable-timeshift
+
+ifeq ($(BR2_PACKAGE_AVAHI),y)
+TVHEADEND_DEPENDENCIES     += avahi
+endif
+
+ifeq ($(BR2_PACKAGE_ZLIB),y)
+TVHEADEND_DEPENDENCIES     += zlib
+endif
+
+ifeq ($(BR2_PACKAGE_LIBCURL),y)
+TVHEADEND_DEPENDENCIES     += curl
+else
+TVHEADEND_CONFIGURE_OPTS   += --disable-imagecache
+endif
+
+define TVHEADEND_CONFIGURE_CMDS
+  (cd $(TVHEADEND_DIR); \
+  ./support/changelog ./debian/changelog buildroot $(TVHEADEND_VERSION); \
+   $(TARGET_CONFIGURE_OPTS) \
+   $(TARGET_CONFIGURE_ARGS) \
+   ./configure \
+  --prefix=/usr \
+  $(TVHEADEND_CONFIGURE_OPTS) \
+  )
+endef
+
+#----------------------------------------------------------------------------
+# To run tvheadend, we need:
+#  - a startup script, and its config file
+#  - a default DB with a tvheadend admin
+define TVHEADEND_INSTALL_DB
+ $(INSTALL) -D package/tvheadend/accesscontrol.1     \
+               $(TARGET_DIR)/root/.hts/tvheadend/accesscontrol/1
+endef
+TVHEADEND_POST_INSTALL_TARGET_HOOKS  = TVHEADEND_INSTALL_DB
+
+define TVHEADEND_INSTALL_INIT_SYSV
+ $(INSTALL) -D $(TVHEADEND_DIR)/debian/tvheadend.default $(TARGET_DIR)/etc/default/tvheadend
+ $(INSTALL) -D $(TVHEADEND_DIR)/debian/tvheadend.init $(TARGET_DIR)/etc/init.d/S99tvheadend
+endef
+
+#----------------------------------------------------------------------------
+# tvheadend is not an autotools-based package, but it is possible to
+# call its ./configure script as if it were an autotools one.
+$(eval $(autotools-package))
